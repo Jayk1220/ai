d2734f3e6f1c996f8b5b4118a596d70323059301
jayk1220 <kimjh4462@gmail.com>
2025-11-18 18:03:27 +0900

    DBMS mySQL

---
+++ source/04_DBMS/1118_ch05_sequence.sql
@@ -0,0 +1,94 @@
+--[V] SEQUENCE :순차번호 생성기. 대부분 인위적인 PK 사용 용도
+
+DROP SEQUENCE FRIEND_SQ;
+CREATE SEQUENCE FRIEND_SQ
+    START WITH 100 --1부터 생성 (기본값:1)
+    INCREMENT BY -2 --1씩 증가 (기본값:1)
+    MAXVALUE 9999 --최대값
+    MINVALUE -9999 --최소값
+    NOCYCLE       -- MAXVALUE 이후에 1로 돌아가지 않고 에러로
+    NOCACHE;      --캐시메모리 사용 안함
+
+SELECT 193+5 FROM DUAL; -- DUAL: 오라클 제공 테이블 (1행 1열)
+SELECT FRIEND_SQ.NEXTVAL FROM DUAL; -- NEXTVAL(순차번호 생성)
+SELECT FRIEND_SQ.CURRVAL FROM DUAL; -- CURRVAL(현재까지 진행된 순차번호)
+
+-- EX 시퀀스를 이용한 FRIEND테이블
+DROP TABLE FRIEND;
+CREATE TABLE FRIEND(
+    NO  NUMBER(5) PRIMARY KEY,
+    NAME VARCHAR2(30) NOT NULL,
+    TEL VARCHAR2(20) UNIQUE,
+    ADDRESS VARCHAR2(255),
+    LAST_MODIFY DATE DEFAULT SYSDATE
+);
+
+DROP SEQUENCE FRIEND_SQ;
+CREATE SEQUENCE FRIEND_SQ MAXVALUE 99999 NOCACHE NOCYCLE;
+
+INSERT INTO FRIEND (NO, NAME, TEL, ADDRESS)
+    VALUES (FRIEND_SQ.NEXTVAL, '홍길동', NULL, '신림'); --여러번 실행 가능
+SELECT NO, NAME, TEL, ADDRESS,
+    TO_CHAR(LAST_MODIFY, 'RR/MM/DD AM HH:MI:SS') "LAST_MODIFY"
+    FROM FRIEND;
+    
+INSERT INTO FRIEND
+    VALUES (FRIEND_SQ.NEXTVAL, '김길동', '010-9999-9999','봉천', SYSDATE);
+    
+    
+    
+--(1) 같은 이름의 테이블이나 시퀀스가 있을 수 있으니 먼저 삭제하고 테이블을 생성하시오
+DROP TABLE MEMBER_LEVEL;
+DROP TABLE MEMBER;
+
+CREATE TABLE MEMBER_LEVEL(
+    LEVELNO     NUMBER(2) PRIMARY KEY,
+    LEVELNAME   VARCHAR2(30) NOT NULL
+);
+
+CREATE TABLE MEMBER (
+    mNO     NUMBER(10)      PRIMARY KEY,
+    mNAME   VARCHAR2(50)    NOT NULL,
+    mPW     VARCHAR2(8)     NOT NULL,
+    mEMAIL  VARCHAR2(100)   UNIQUE NOT NULL,
+    mPOINT  NUMBER(10)      DEFAULT 0 CHECK (mPOINT >=0),
+    mRDATE  DATE            DEFAULT SYSDATE,
+    LEVELNO NUMBER(2)
+);
+
+--(2) MEMBER 테이블은 필드 별로 다음의 조건을 지켜 생성하시오.
+/*	① mNO(번호) : 주키
+	② mNAME(이름) : NULL값을 입력할 수 없다
+	③ mPW(비밀번호) : 반드시 1~8글자 이내의 문자를 입력한다
+	④ mEMAIL(메일) : 모든 데이터는 EMAIL 필드값이 모두 다르게 입력된다.
+	⑤ mPOINT(포인트) : 0이상의 값만 입력할 수 있다.
+	⑥ mRDATE(가입일) : 입력하지 않을 시, 기본적으로 현재날짜로 입력된다.
+⑦ LEVELNO : MEMBER_LEVEL 테이블의 LEVELNO 필드를 참조하는 외래키
+(3) MEMBER_LEVEL 테이블은 필드 별로 다음의 제약조건을 지킨다.
+	① LEVELNO : 주키
+	② LEVELNAME : NULL값을 입력할 수 없다	
+(4) MEMBER 테이블의 mNO번호는 시퀀스(MEMBER_MNO_SQ)를 생성한 뒤 자동생성 번호로 입력
+*/
+
+DROP SEQUENCE mNO_NUM;
+CREATE SEQUENCE mNO_NUM
+    START WITH 1
+    MAXVALUE 100000
+    NOCYCLE;
+    
+INSERT INTO MEMBER_LEVEL VALUES (-1, 'black');
+INSERT INTO MEMBER_LEVEL VALUES (0, '일반');
+INSERT INTO MEMBER_LEVEL VALUES (1, '실버');
+INSERT INTO MEMBER_LEVEL VALUES (2, '골드');
+
+SELECT * FROM MEMBER_LEVEL;
+
+INSERT INTO MEMBER VALUES (mNO_NUM.NEXTVAL, '홍길동', 'aa', 'hong@hong.com',0,'24/09/26',0);
+INSERT INTO MEMBER VALUES (mNO_NUM.NEXTVAL, '신길동', 'bb', 'sin@sin.com',1000,'22/04/01',1);
+
+SELECT * FROM MEMBER;
+
+--출력내용
+SELECT  mNO, mNAME, TO_CHAR(mRDATE,'YYYY-MM-DD') mRDATE, mEMAIL, mPOINT point, LEVELNAME||'고객' levelname
+    FROM MEMBER M, MEMBER_LEVEL N
+    WHERE M.LEVELNO = N.LEVELNO;
