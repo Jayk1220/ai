537b6ea3d1fea6c8a0be2d03697ff10fa8107e42
Jayk4462 <kimjh4462@gmail.com>
2026-01-15 01:01:34 +0900

    django

--- source/12_django/ch01_hello/home/views.py
+++ source/12_django/ch01_hello/home/views.py
@@ -1,5 +1,7 @@
+# home/views.py
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, template_name: "home.html", context: {"msg":"Django(장고)"})
